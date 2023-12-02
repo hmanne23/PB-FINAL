e56e97ac2b587818694e2539f73b7aeecb25cf1a
hmanne23 <rahulchand810@gmail.com>
2023-12-02 10:59:43 -0500

    Addded all functionalities

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,21 +1,53 @@
-
-import { Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useAuth } from "../AuthContext";
+import { LogoutPage } from "../LogoutPage/LogoutPage";
 
 function Menu() {
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li><Link to="/">Homepage</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/login">Login</Link></li>
-                
-                </ul>
-            </nav>
-        </div>
-        
-  
-    );
-  }
-  
-  export default Menu;
+  const { isLoggedIn, logout } = useAuth();
+  const handlelogout = () => {
+    logout();
+    localStorage.removeItem("userId");
+    localStorage.removeItem("token");
+  };
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+
+          {isLoggedIn ? (
+            <>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link to="/Visual">Visuals</Link>
+              </li>
+              <li>
+                <Link to="/login" onClick={handlelogout}>
+                  Logout
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Signup</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Menu;
